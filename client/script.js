ba9cb94bf0b4188fdfb3fcaf97a66dbb9195a62a
renderer-design <manuelajgampa@gmail.com>
2023-03-27 00:41:46 +0200

    ahh commit

--- client/script.js
+++ client/script.js
@@ -70,7 +70,7 @@ const handleSubmit = async (e) => {
 	const messageDiv = document.getElementById(uniqueId);
 	loader(messageDiv);
 	
-	const response = await fetch('http://localhost:5000/',{
+	const response = await fetch('https://davider.onrender.com/',{
 	method: 'POST',
 	headers: {
 		'Content-Type': 'application/json'
