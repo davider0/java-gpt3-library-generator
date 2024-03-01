587412c9a2a0e9cd578a22a94f753eab97c194c0
renderer-design <manuelajgampa@gmail.com>
2024-03-01 18:57:30 +0100

    final fix

--- client/script.js
+++ client/script.js
@@ -85,7 +85,7 @@ const handleSubmit = async (e) => {
 	if(response.ok){
 		const data = await response.json();
 		console.log("response status 200 (⌐■_■)");
-		const parsedData = data.bot.toString().split(": ")[0].replace(/[{}":\\\n]/g,"") + ": " + data.bot.toString().split(": ")[1].replace(/[\\]/g,"").substring(1, data.bot.toString().split(": ")[1].length-2);
+		const parsedData = data.bot.toString().split(": ")[0].replace(/[{}":\\\n]/g,"") + ": " + data.bot.toString().split(": ")[1].replace(/[\\]/g,"").substring(1, data.bot.toString().split(": ")[1].length-3);
 		
 		typeText(messageDiv, parsedData);
 		
