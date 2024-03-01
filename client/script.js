444c8165c2c446f953a5e155f4f065600191c00b
renderer-design <manuelajgampa@gmail.com>
2024-03-01 17:43:03 +0100

    typo fix

--- client/script.js
+++ client/script.js
@@ -83,7 +83,7 @@ const handleSubmit = async (e) => {
 	messageDiv.innerHTML = ''
 	if(response.ok){
 		const data = await response.json();
-		const parsedData = solve(data.bot).Response;
+		const parsedData = eval(data.bot).Response;
 		console.log(data);
 		typeText(messageDiv, parsedData);
 		
