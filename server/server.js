aac97e20e2d48a9f35266002cd0cc20a615039d8
renderer-design <manuelajgampa@gmail.com>
2024-03-01 17:26:54 +0100

    almost there

--- server/server.js
+++ server/server.js
@@ -58,7 +58,7 @@ app.post('/', async (req,res) => {
 		});
 		
 		res.status(200).send({
-			bot: response.data.choices[0].message
+			bot: response.data.choices[0].message.response
 		});
 
 		// Esperar antes de hacer la próxima solicitud
